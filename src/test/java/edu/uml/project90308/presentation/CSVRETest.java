1a1eaf7ed9c01a8aa48c217a9a5ef7a549ec715e
Peter G. Martin <pg.martin@rcn.com>
2012-11-02 22:54:06 -0400

    src/test/java/edu/uml/project90308/presentation/CSVRETest.java

---
+++ src/test/java/edu/uml/project90308/presentation/CSVRETest.java
@@ -0,0 +1,31 @@
+package edu.uml.project90308.presentation;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import edu.uml.project90308.presentation.*;
+
+public class CSVRETest {
+
+    List<String> testString = new ArrayList<String>();
+    String testCSV = "\"one\",two,three,";
+
+     @Before
+    public void setUp() {
+        testString.add("one");
+        testString.add("two");
+        testString.add("three");
+      }
+
+    @Test
+    public void testParse() {
+        CSVRE csvre = new CSVRE();
+        List<String> res = csvre.parse(testCSV);
+        assertEquals(testString, res);
+    }
+
+}
